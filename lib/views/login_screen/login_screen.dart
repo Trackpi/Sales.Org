4bee66ddae87e800e9c7f5694116cb52d763ab64
kcaju <Arjooonz@gmail.com>
2024-12-17 17:34:35 +0530

    login screen done

--- lib/views/login_screen/login_screen.dart
+++ lib/views/login_screen/login_screen.dart
@@ -5,6 +5,223 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 45,
+                ),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    color: Color(0xFFFCB205),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //Company logo
+                Image.asset(
+                  "assets/images/logo.png",
+                  height: 96,
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "Your Strategic Growth Partner",
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 73,
+                ),
+                //two textfields
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFFFFFFF),
+                    hintText: "Employer ID",
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Color(0xFFFFFFFF),
+                    hintText: "Password",
+                    suffixIcon: Icon(
+                      Icons.visibility_off_outlined,
+                      color: Colors.black,
+                    ),
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                      fontWeight: FontWeight.normal,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        //forgot passwor alertbox popup
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Container(
+                                width: 340,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      "Forgot Password ?",
+                                      style: TextStyle(
+                                        color: Color(0xFF000000),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 17,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Text(
+                                      textAlign: TextAlign.center,
+                                      "If you Forget Password \n please check your offer letter or \n contact your manager !!",
+                                      style: TextStyle(
+                                        color: Color(0xFF000000),
+                                        fontWeight: FontWeight.normal,
+                                        fontSize: 13,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 16,
+                                    ),
+                                    Divider(
+                                      thickness: 2,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              actions: [
+                                Center(
+                                  child: InkWell(
+                                    onTap: () {
+                                      //to pop back from alertbox
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "OK",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                      child: Text(
+                        "Forgot Password ?",
+                        style: TextStyle(
+                          color: Color(0xFFFCB205),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                //login button
+                Container(
+                  alignment: Alignment.center,
+                  height: 50,
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(
+                      color: Color(0xFFFFFFFF),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Color(0xFFFCB205),
+                  ),
+                ),
+                SizedBox(
+                  height: 198,
+                ),
+                //navigate to register screen
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(
+                        color: Color(0xFFFFFFFF),
+                        fontWeight: FontWeight.normal,
+                        fontSize: 18,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Color(0xFFFCB205),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 59,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
