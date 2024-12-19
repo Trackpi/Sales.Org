1711b427157610bd98a00080a171558c6e34a74e
cbabi2023 <abijithcbbsccs2021@gmail.com>
2024-12-19 12:53:19 +0530

    updated last

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -8,7 +8,7 @@ class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen>
@@ -20,25 +20,16 @@ class _SplashScreenState extends State<SplashScreen>
   @override
   void initState() {
     super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3))
+          ..forward();
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    )..forward();
-
-    // Trigger text and button fade-in after wave animation completes
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
-        Future.delayed(const Duration(milliseconds: 300), () {
-          setState(() {
-            _textOpacity = 1.0;
-          });
-        });
-        Future.delayed(const Duration(milliseconds: 800), () {
-          setState(() {
-            _buttonOpacity = 1.0;
-          });
-        });
+        Future.delayed(const Duration(milliseconds: 300),
+            () => setState(() => _textOpacity = 1.0));
+        Future.delayed(const Duration(milliseconds: 800),
+            () => setState(() => _buttonOpacity = 1.0));
       }
     });
   }
@@ -54,39 +45,29 @@ class _SplashScreenState extends State<SplashScreen>
     return AnimatedBuilder(
       animation: _controller,
       builder: (context, child) {
+        final waveHeight = 60 * (1 - _controller.value);
         return Scaffold(
           backgroundColor: _controller.value >= 1.0
               ? const Color(0xffFE8900)
               : Colors.transparent,
           body: Stack(
             children: [
-              // Wave animation
               CustomPaint(
-                painter: WavePainter(progress: _controller.value),
+                painter: WavePainter(
+                    progress: _controller.value, waveHeight: waveHeight),
                 child: const SizedBox.expand(),
               ),
-
-              // Zoom-in logo animation from zero size
               Center(
                 child: TweenAnimationBuilder<double>(
                   tween: Tween<double>(begin: 0.0, end: 1.0),
                   duration: const Duration(seconds: 2),
                   curve: Curves.easeIn,
-                  builder: (context, scale, child) {
-                    return Transform.scale(
-                      scale: scale,
-                      child: child,
-                    );
-                  },
-                  child: Image.asset(
-                    Assets.png.logo.path, // Replace with your asset path
-                    width: 200,
-                    height: 200,
-                  ),
+                  builder: (context, scale, child) =>
+                      Transform.scale(scale: scale, child: child),
+                  child: Image.asset(Assets.png.logo.path,
+                      width: 200, height: 200),
                 ),
               ),
-
-              // Fade-in text below the logo
               AnimatedOpacity(
                 opacity: _textOpacity,
                 duration: const Duration(seconds: 1),
@@ -94,20 +75,17 @@ class _SplashScreenState extends State<SplashScreen>
                 child: Align(
                   alignment: Alignment.center,
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 180),
-                    child: Text(
+                    padding: const EdgeInsets.only(top: 250),
+                    child: const Text(
                       "Your Strategic Growth Partner",
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     ),
                   ),
                 ),
               ),
-
-              // Fade-in "Get Started" button
               AnimatedOpacity(
                 opacity: _buttonOpacity,
                 duration: const Duration(seconds: 1),
@@ -117,25 +95,21 @@ class _SplashScreenState extends State<SplashScreen>
                   child: Padding(
                     padding: const EdgeInsets.only(bottom: 50.0),
                     child: GestureDetector(
-                      onTap: () {
-                        // Add navigation logic here
-                        Navigator.pushNamed(context, AppRoutes.loginScreen);
-                      },
+                      onTap: () =>
+                          Navigator.pushNamed(context, AppRoutes.loginScreen),
                       child: Container(
                         alignment: Alignment.center,
                         width: 313,
                         height: 49,
                         decoration: BoxDecoration(
-                          color: const Color(0xFFFFFFFF),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8)),
                         child: const Text(
                           "Get Started",
                           style: TextStyle(
-                            color: Color(0xffFE8900),
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
+                              color: Color(0xffFE8900),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                     ),
@@ -152,31 +126,27 @@ class _SplashScreenState extends State<SplashScreen>
 
 class WavePainter extends CustomPainter {
   final double progress;
+  final double waveHeight;
 
-  WavePainter({required this.progress});
+  WavePainter({required this.progress, required this.waveHeight});
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
       ..shader = const LinearGradient(
-        colors: [
-          Color(0xffFEDC3F),
-          Color(0xffFE8900),
-        ],
-        begin: Alignment.bottomCenter,
-        end: Alignment.topCenter,
+        colors: [Color(0xffFEDC3F), Color(0xffFE8900)],
+        begin: Alignment.centerRight,
+        end: Alignment.centerLeft,
       ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
 
     final path = Path();
-
-    double waveHeight = 30;
-    double waveProgress = size.height * (1 - progress);
+    final waveProgress = size.height * (1 - progress);
 
     path.moveTo(0, size.height);
     path.lineTo(0, waveProgress);
 
     for (double x = 0; x <= size.width; x += size.width / 20) {
-      double y = waveProgress +
+      final y = waveProgress +
           waveHeight * math.sin(2 * math.pi * (x / size.width + progress));
       path.lineTo(x, y);
     }
