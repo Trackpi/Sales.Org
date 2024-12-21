e0cb0ca8ea7b75ad922ee222564bc0db078c5499
gokuljnair2001 <gokulnandhuse65@gmail.com>
2024-12-21 23:11:03 +0530

    FAQ saved screen added

--- lib/views/faqs_screen/faq_saved_screen.dart
+++ lib/views/faqs_screen/faq_saved_screen.dart
@@ -1,1 +1,129 @@
 //for gokul
+
+import 'package:flutter/material.dart';
+
+class FaqSavedScreen extends StatelessWidget {
+  const FaqSavedScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 30,
+        ),
+        title: Text(
+          'FAQs',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        actions: [
+          Icon(
+            Icons.star,
+            size: 30,
+            color: Colors.orange,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 15),
+          Center(
+            child: Text(
+              'How can we help you?',
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: Colors.white),
+                  suffixIcon: Icon(Icons.mic, color: Colors.white),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: Colors.grey),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Saved FAQs',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 15,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.history,
+                            color: Colors.white,
+                            size: 26,
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Text(
+                              'How to add a payment method by this app?',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.star, color: Colors.orange),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      color: Colors.white.withAlpha(90),
+                      thickness: .9,
+                      indent: 20,
+                      endIndent: 20,
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
