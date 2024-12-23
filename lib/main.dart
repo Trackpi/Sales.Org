9dfc53eb65c99f1ac64c71aa9d25dc40f70d4604
cbabi2023 <abijithcbbsccs2021@gmail.com>
2024-12-23 12:21:52 +0530

    sizer updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sales_org/routes/app_routes.dart';
 import 'package:sales_org/theme/theme_data.dart';
+import 'package:sales_org/utils/size_utils.dart';
 import 'package:sales_org/views/splash_screen/splash_screen.dart';
 
 void main() {
@@ -17,6 +18,10 @@ class MyApp extends StatelessWidget {
       onGenerateRoute: AppRoutes.generateRoute,
       initialRoute: AppRoutes.splashScreen,
       debugShowCheckedModeBanner: false,
+      builder: (context, child) => Sizer(
+          builder: (context, Orientation orientation, DeviceType deviceType) {
+        return child ?? const SizedBox();
+      }),
       home: SplashScreen(),
     );
   }
