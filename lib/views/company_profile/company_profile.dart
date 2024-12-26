3f9e680688e9e40b24d3cd74d2dd6dae95ad53bb
kcaju <Arjooonz@gmail.com>
2024-12-26 20:33:37 +0530

    companyprofile done

--- lib/views/company_profile/company_profile.dart
+++ lib/views/company_profile/company_profile.dart
@@ -7,10 +7,31 @@ class CompanyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+      //appbar
       appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+        ),
         centerTitle: true,
-        title: Text('Company Profile'),
+        title: Text(
+          "Company Profile",
+          style: TextStyle(
+              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
         actions: [
+          const Icon(
+            Icons.quiz_outlined,
+            color: Colors.white,
+          ),
+          SizedBox(width: SizeUtils.width * 0.015),
+          const Icon(
+            Icons.bookmark_outline,
+            color: Colors.white,
+          ),
+          SizedBox(width: SizeUtils.width * 0.03),
           const Icon(Icons.quiz_outlined),
           SizedBox(
             // width: 10
@@ -22,6 +43,339 @@ class CompanyProfile extends StatelessWidget {
             width: SizeUtils.width * 0.02,
           ),
         ],
+      ),
+      //body
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: SizeUtils.width * 0.04,
+              vertical: SizeUtils.height * 0.03),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //company image
+              Container(
+                height: SizeUtils.height * 0.228,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                            "https://media.licdn.com/dms/image/v2/D560BAQEDGA0mgxzP2g/company-logo_200_200/company-logo_200_200/0/1726567177130/trackpi_private_limited_logo?e=2147483647&v=beta&t=i11jD7Z7tqj9Qwr59_97UddJcghokMqZNp3PPtLwd4g"))),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.015,
+              ),
+              Text(
+                "Company Name",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.01,
+              ),
+              Text(
+                "Type of Company",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.01,
+              ),
+              Text(
+                "Tons of awesome MERN Stack wallpapers to download for free. You can also upload and share your favorite MERN Stack",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.03,
+              ),
+              //brochure
+              Container(
+                  height: SizeUtils.height * 0.2,
+                  child: Column(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: SizeUtils.width * 0.04,
+                              vertical: SizeUtils.height * 0.012),
+                          alignment: Alignment.topRight,
+                          height: SizeUtils.height * 0.16,
+                          child: Icon(
+                            Icons.bookmark,
+                            size: 30,
+                            color: Color(0xFFFCB205),
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xFF2A2A2A),
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(10)),
+                          )),
+                      Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: SizeUtils.width * 0.035,
+                            vertical: SizeUtils.height * 0.005),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Brochure Name",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.normal),
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.download_outlined,
+                              size: 25,
+                              color: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.share,
+                              size: 20,
+                              color: Colors.white,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFCB205),
+                    borderRadius: BorderRadius.circular(10),
+                  )),
+              SizedBox(
+                height: SizeUtils.height * 0.03,
+              ),
+              //products
+              Row(
+                children: [
+                  Text(
+                    "Products",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Spacer(),
+                  Text(
+                    "View More",
+                    style: TextStyle(
+                        color: Color(0xFFFCB205),
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.02,
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.20,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: SizeUtils.width * 0.02),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: SizeUtils.width * 0.47,
+                            height: SizeUtils.height * 0.15,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                  "https://images.prismic.io/loco-blogs/79328284-f97b-489f-924c-eb3b17e34b56_image2.png?auto=compress%2Cformat&rect=0%2C0%2C1999%2C1124&w=1920&h=1080&ar=1.91%3A1",
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: SizeUtils.height * 0.01,
+                          ),
+                          Text(
+                            "Product Name",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.03,
+              ),
+              //promotion materials
+              Row(
+                children: [
+                  Text(
+                    "Promotion Materials",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Spacer(),
+                  Text(
+                    "View More",
+                    style: TextStyle(
+                        color: Color(0xFFFCB205),
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.02,
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.15,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: SizeUtils.width * 0.02),
+                      child: Container(
+                        width: SizeUtils.width * 0.47,
+                        height: SizeUtils.height * 0.15,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              "https://media.licdn.com/dms/image/v2/D560BAQEDGA0mgxzP2g/company-logo_200_200/company-logo_200_200/0/1726567177130/trackpi_private_limited_logo?e=2147483647&v=beta&t=i11jD7Z7tqj9Qwr59_97UddJcghokMqZNp3PPtLwd4g",
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.03,
+              ),
+              //testimonials materials
+              Row(
+                children: [
+                  Text(
+                    "Testimonials Materials",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Spacer(),
+                  Text(
+                    "View More",
+                    style: TextStyle(
+                        color: Color(0xFFFCB205),
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.02,
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.15,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: SizeUtils.width * 0.02),
+                      child: Container(
+                        width: SizeUtils.width * 0.47,
+                        height: SizeUtils.height * 0.15,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              "https://media.licdn.com/dms/image/v2/D560BAQEDGA0mgxzP2g/company-logo_200_200/company-logo_200_200/0/1726567177130/trackpi_private_limited_logo?e=2147483647&v=beta&t=i11jD7Z7tqj9Qwr59_97UddJcghokMqZNp3PPtLwd4g",
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.03,
+              ),
+              //posters
+              Row(
+                children: [
+                  Text(
+                    "Posters",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  Spacer(),
+                  Text(
+                    "View More",
+                    style: TextStyle(
+                        color: Color(0xFFFCB205),
+                        fontSize: 12,
+                        fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.02,
+              ),
+              SizedBox(
+                height: SizeUtils.height * 0.19,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: EdgeInsets.only(right: SizeUtils.width * 0.025),
+                      child: Container(
+                        width: SizeUtils.width * 0.35,
+                        height: SizeUtils.height * 0.15,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              "https://media.licdn.com/dms/image/v2/D560BAQEDGA0mgxzP2g/company-logo_200_200/company-logo_200_200/0/1726567177130/trackpi_private_limited_logo?e=2147483647&v=beta&t=i11jD7Z7tqj9Qwr59_97UddJcghokMqZNp3PPtLwd4g",
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
