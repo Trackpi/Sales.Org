7b851e0773e0fd8b2fd1d7a4d4010f4ecc3dcebe
deekshith <deekshithrajesh29@.com>
2024-12-28 12:03:59 +0530

    resignation screen updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sales_org/routes/app_routes.dart';
 import 'package:sales_org/theme/theme_data.dart';
 import 'package:sales_org/utils/size_utils.dart';
-
 import 'package:sales_org/views/splash_screen/splash_screen.dart';
 
 void main() {
@@ -15,15 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: AppTheme.darkTheme,
-      onGenerateRoute: AppRoutes.generateRoute,
-      initialRoute: AppRoutes.splashScreen,
-      debugShowCheckedModeBanner: false,
-      builder: (context, child) => Sizer(
-          builder: (context, Orientation orientation, DeviceType deviceType) {
-        return child ?? const SizedBox();
-      }),
-      home: SplashScreen(),
-    );
+        theme: AppTheme.darkTheme,
+        onGenerateRoute: AppRoutes.generateRoute,
+        initialRoute: AppRoutes.splashScreen,
+        debugShowCheckedModeBanner: false,
+        builder: (context, child) => Sizer(builder:
+                (context, Orientation orientation, DeviceType deviceType) {
+              return child ?? const SizedBox();
+            }),
+        home: SplashScreen());
   }
 }
--- lib/views/profile_screens/resignationform_screen.dart
+++ lib/views/profile_screens/resignationform_screen.dart
@@ -1,185 +1,215 @@
-//for deekshith
-
 import 'package:flutter/material.dart';
 import 'package:sales_org/utils/size_utils.dart';
 
+// This widget represents the Resignation Form screen.
 class ResignationformScreen extends StatelessWidget {
   const ResignationformScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.black, // Sets the background color of the screen.
       appBar: AppBar(
         leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            )),
-        centerTitle: true,
+          onPressed: () {}, // Action when back button is pressed.
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white, // White color for the back icon.
+          ),
+        ),
+        centerTitle: true, // Centers the title in the AppBar.
         actions: [
           IconButton(
-              icon: Icon(
-                Icons.notifications_outlined,
-                color: Colors.white,
-              ),
-              onPressed: () {})
+            icon: Icon(
+              Icons.notifications_outlined,
+              color: Colors.white, // White color for the notifications icon.
+            ),
+            onPressed: () {}, // Action when notifications icon is pressed.
+          ),
         ],
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.black, // Black AppBar background.
         title: Text(
-          "Resignation",
+          "Resignation", // Title displayed in the AppBar.
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            color: Colors.white,
-            fontSize: 20,
+            color: Colors.white, // White color for the title text.
+            fontSize: 20, // Font size for the title text.
           ),
         ),
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(
-            vertical: SizeUtils.height * 0.01,
-            horizontal: SizeUtils.width * 0.02,
+            vertical: SizeUtils.height * 0.01, // Vertical padding.
+            horizontal: SizeUtils.width * 0.02, // Horizontal padding.
           ),
           child: Column(
             children: [
-              // SizedBox(
-              //   height: 20,
-              // ),
+              // Title of the form.
               Center(
                 child: Text(
                   "Resignation Form",
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+                    color: Colors.white, // White color for the text.
+                    fontSize: 15, // Font size for the text.
                   ),
                 ),
               ),
               SizedBox(
-                height: SizeUtils.height * 0.022,
+                height: SizeUtils.height * 0.022, // Space below the title.
               ),
               Container(
                 padding: EdgeInsets.symmetric(
-                  vertical: SizeUtils.height * 0.012,
-                  horizontal: SizeUtils.width * 0.03,
+                  vertical: SizeUtils.height * 0.012, // Vertical padding.
+                  horizontal: SizeUtils.width * 0.03, // Horizontal padding.
                 ),
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  crossAxisAlignment:
+                      CrossAxisAlignment.start, // Aligns items to the start.
                   children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment
+                          .spaceBetween, // Spreads items evenly.
                       children: [
+                        // Displays "From" text.
                         Text(
                           "From",
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
+                            color: Colors.white, // White color for text.
+                            fontSize: 13, // Font size for text.
                           ),
                         ),
+                        // Displays the date.
                         Text(
                           "20/12/2024",
                           style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 13,
+                            color: Colors.white, // White color for text.
+                            fontSize: 13, // Font size for text.
                           ),
                         ),
                       ],
                     ),
                     SizedBox(
-                      height: SizeUtils.height * 0.02,
+                      height: SizeUtils.height *
+                          0.02, // Space below "From" section.
                     ),
+                    // Displays name of the sender.
                     Text(
                       "Paul Walker",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15,
+                        color: Colors.white, // White color for text.
+                        fontSize: 15, // Font size for text.
                       ),
                     ),
+                    // Displays the designation of the sender.
                     Text(
                       "Designation",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
+                        color: Colors.white, // White color for text.
+                        fontSize: 14, // Font size for text.
                       ),
                     ),
                     SizedBox(
-                      height: SizeUtils.height * 0.02,
+                      height:
+                          SizeUtils.height * 0.02, // Space before "To" section.
                     ),
+                    // Displays "To" text.
                     Text(
                       "To",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13,
+                        color: Colors.white, // White color for text.
+                        fontSize: 13, // Font size for text.
                       ),
                     ),
                     SizedBox(
-                      height: SizeUtils.height * 0.02,
+                      height:
+                          SizeUtils.height * 0.02, // Space below "To" section.
                     ),
+                    // Displays name of the recipient.
                     Text(
                       "Tom Wilson",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
+                        color: Colors.white, // White color for text.
+                        fontSize: 14, // Font size for text.
                       ),
                     ),
+                    // Displays the designation of the recipient.
                     Text(
                       "Manager",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
+                        color: Colors.white, // White color for text.
+                        fontSize: 14, // Font size for text.
                       ),
                     ),
+                    // Text field for additional information.
                     TextFormField(),
                     SizedBox(
-                      height: SizeUtils.height * 0.03,
+                      height: SizeUtils.height *
+                          0.03, // Space after the text field.
                     ),
+                    // Label for reason text field.
                     Text(
                       "Reason for resignation",
                       style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontSize: 14,
+                        color: Colors.white
+                            .withAlpha(100), // Semi-transparent white text.
+                        fontSize: 14, // Font size for text.
                       ),
                     ),
+                    // Text field for reason of resignation.
                     TextFormField(),
                     SizedBox(
-                      height: SizeUtils.height * 0.02,
+                      height:
+                          SizeUtils.height * 0.02, // Space below text field.
                     ),
+                    // Label for explanation text field.
                     Text(
                       "Explanation for resignation",
                       style: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
-                        fontSize: 14,
+                        color: Colors.white
+                            .withAlpha(100), // Semi-transparent white text.
+                        fontSize: 14, // Font size for text.
                       ),
                     ),
                   ],
                 ),
                 decoration: BoxDecoration(
-                  color: Color(0xff2A2A2A),
-                  borderRadius: BorderRadius.circular(10),
+                  color:
+                      Color(0xff2A2A2A), // Background color for the container.
+                  borderRadius: BorderRadius.circular(10), // Rounded corners.
                 ),
-                height: 524,
-                width: 353,
+                height: SizeUtils.height * 0.6, // Height of the container.
+                width: SizeUtils.width * 0.9, // Width of the container.
               ),
               SizedBox(
-                height: 20,
+                height:
+                    SizeUtils.height * 0.03, // Space before the submit button.
               ),
-              Container(
-                height: 30,
-                width: 135,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                    Color(0xffFEDC3F),
-                    Color(0xffFE8900),
-                  ], begin: Alignment.centerLeft, end: Alignment.centerRight),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Text(
-                    "Submit",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
+              InkWell(
+                onTap: () {
+                  // Action when the submit button is pressed.
+                },
+                child: Container(
+                  height: SizeUtils.height * 0.04, // Height of the button.
+                  width: SizeUtils.width * 0.3, // Width of the button.
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xffFEDC3F), // Gradient start color.
+                        Color(0xffFE8900), // Gradient end color.
+                      ],
+                      begin: Alignment.centerLeft, // Gradient starting point.
+                      end: Alignment.centerRight, // Gradient ending point.
+                    ),
+                    borderRadius: BorderRadius.circular(10), // Rounded corners.
+                  ),
+                  child: Center(
+                    child: Text(
+                      "Submit", // Submit button text.
+                      style: TextStyle(
+                        color: Colors.white, // White color for text.
+                        fontSize: 15, // Font size for text.
+                      ),
                     ),
                   ),
                 ),
