13d514a5c71a2fc844b6e5bb2935d6573b9e335b
gokuljnair2001 <gokulnandhuse65@gmail.com>
2025-01-13 21:14:14 +0530

    todo list complete screen added

--- lib/views/todo_list_page/todo_completedlist.dart
+++ lib/views/todo_list_page/todo_completedlist.dart
@@ -1,1 +1,122 @@
 //for gokul
+import 'package:flutter/material.dart';
+import 'package:sales_org/utils/size_utils.dart';
+
+class TodolistComplete extends StatelessWidget {
+  const TodolistComplete({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.black,
+        title: Text('To Do List', style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {}, // Add your back button functionality here
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search, color: Colors.white),
+            onPressed: () {}, // Add your search functionality here
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Completed',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: SizeUtils.height * 0.019),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 6, // Adjust the number of items as needed
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(bottom: 12.0),
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[850],
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'Meeting',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                '20/12/2024',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              SizedBox(
+                                width: SizeUtils.width * 0.009,
+                              ),
+                              Text(
+                                '-',
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.white),
+                              ),
+                              SizedBox(
+                                width: SizeUtils.width * 0.008,
+                              ),
+                              Text(
+                                '10:00 AM',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: SizeUtils.height * 0.007),
+                          Text(
+                            'At the Company',
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 14,
+                            ),
+                          ),
+                          SizedBox(height: SizeUtils.height * 0.019),
+                          Text(
+                            'Lorem ipsum dolor sit amet consectetur. Ipsum dui nulla.',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
